ab9d9f1a7d068471c8efc3f555204dd319c9fd05
Wenfang Du <duwenfangcn@163.com>
2021-05-19 14:24:50 +0800

    added repro

--- README.md
+++ README.md
@@ -1,24 +1,3 @@
 # vue-3-tsx-hmr-repro
 
-## Project setup
-```
-npm install
-```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+Edit `App.tsx`, browser page refreshes, no hot module replacement.
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { defineComponent } from '@vue/runtime-core'
+
+export default defineComponent({
+  setup() {
+    return () => (
+      <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+        cupiditate voluptates deleniti consectetur, omnis blanditiis lum sequi
+        suscipit dicta sunt fugiat voluptas est necessitatibus nostrum. Atque,
+        eligendi praesentium. Pariatur, nobis.
+      </div>
+    )
+  },
+})
